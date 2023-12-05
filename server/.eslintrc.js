cd9b06a233749b929b4bb01972d9e6ff4f9985f8
Yanlex <takashi21@yandex.ru>
2023-12-05 18:02:01 +0300

    eslint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 2020,
-		sourceType: 'module',
+		sourceType: 'commonjs',
 		ecmaFeatures: {
 			jsx: true,
 		},
